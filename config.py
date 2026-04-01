6f4b1dc43f2e45f55e81719f05497c556a9c94d5
momoF07 <little.skylander@gmail.com>
2026-04-02 01:01:31 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+from datetime import time
+
+# Amplitude horaire du parc (Heure d'ouverture et de fermeture prévue)
+# Cela permet de gérer l'affichage "Fermé" de manière plus propre
+PARK_OPENING = time(8, 15)   # 08:15
+PARK_CLOSING = time(23, 0)  # 23:00
+
+# Note : Tu peux modifier ces valeurs ici quand les saisons changent
