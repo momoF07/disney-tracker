259fb3eb46385ad476d5e38fe17419b0766904ce
momoF07 <little.skylander@gmail.com>
2026-04-05 02:33:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -319,8 +319,7 @@ if not df_raw.empty:
         else: st.write("✅ Aucune interruption détectée.")
 
     else: st.warning("⏳ En attente des premières données de la journée.")
-else: st.warning(f"📭 Aucune donnée disponible.\n
-Merci de patienter jusqu'à {PARK_OPENING}.")
+else: st.warning(f"📭 Aucune donnée disponible.\nMerci de patienter jusqu'à {PARK_OPENING}.")
 
 st.divider()
 st.caption("Disney Wait Time Tool")
