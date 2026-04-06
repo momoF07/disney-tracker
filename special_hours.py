439c9833f89afae1405a0c548f66928aca92d9c5
momoF07 <little.skylander@gmail.com>
2026-04-06 12:32:21 +0200

    Update special_hours.py

--- special_hours.py
+++ special_hours.py
@@ -6,15 +6,16 @@ ANTICIPATED_CLOSINGS = {
     "Le Passage Enchanté d'Aladdin": time(22, 15),
     "Pirates' Beach": time(22, 15),
     "Rustler Roundup Shootin' Gallery": time(21, 35),
+    "Cars ROAD TRIP": time(21, 40),
     "La Tanière du Dragon": time(20, 55),
-    "Alice's Curious Labyrinth": time(20, 25),
-    "Casey Jr. – le Petit Train du Cirque": time(20, 25),
-    "Disneyland Railroad Frontierland Depot": time(20, 25),
-    "Disneyland Railroad Main Street Station": time(20, 25),
+    "Alice's Curious Labyrinth": time(20, 0),
+    "Casey Jr. – le Petit Train du Cirque": time(20, 0),
+    "Disneyland Railroad Frontierland Depot": time(19, 45),
+    "Disneyland Railroad Main Street Station": time(19, 45),
     "Frontierland Playground": time(19, 55),
-    "Le Pays des Contes de Fées, presented by Vittel": time(19, 55),
-    "Thunder Mesa Riverboat Landing": time(17, 15),
-    "Cars ROAD TRIP": time(21, 35),
+    "Le Pays des Contes de Fées, presented by Vittel": time(20, 0),
+    "Discoveryland Theater": time(17, 30),
+    "Thunder Mesa Riverboat Landing": time(17, 0),
     
 }
 
@@ -26,4 +27,21 @@ FANTASYLAND_EARLY_CLOSE = [
     "Le Carrousel de Lancelot",
     "Blanche-Neige et les Sept Nains®",
     "Les Voyages de Pinocchio",
+]
+
+EMT_EARLY_OPEN = [
+    "Big Thunder Mountain",
+    "Peter Pan's Flight", 
+    "Dumbo The Flying Elephant", 
+    "HyperSpace Mountain", 
+    "Buzz Lightyear Laserblast",
+    "Main Street Vehicles",
+    "Crush's Coaster",
+    "WEB", 
+    "Flight Force",
+    "Tour de la Terreur", 
+    "Slinky Dog", 
+    "Ratatouille",
+    "Frozen Ever After",
+    "Raiponce Tangled Spin",
 ]
