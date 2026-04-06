f6c4802fd5ce8c57cb138f0ded61c4ff80dbcc3d
momoF07 <little.skylander@gmail.com>
2026-04-06 19:58:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -213,6 +213,7 @@ if not df_live.empty:
                                 if p['statut'] == "TERMINEE": st.caption(f"• 🟢 :green[**Ope à {p['fin'].strftime('%H:%M')}**] | 🔴 :red[**Panne à {h_d}**] ({p['duree']} min)")
                     else: st.write("✅ **Aucun incident signalé aujourd'hui**")
             st.write("")
+            st.divider()
 
 # --- DERNIÈRES INTERRUPTIONS ---
 st.subheader("🚨 Dernières interruptions")
