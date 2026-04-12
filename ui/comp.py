8f1a9e9e037faa1fda6708b5eeee58a062f7ae11
momoF07 <little.skylander@gmail.com>
2026-04-12 19:00:36 +0200

    .

--- ui/comp.py
+++ ui/comp.py
@@ -34,7 +34,7 @@ def render_weather_card(weather):
             </div>
             <div style="text-align: right;">
                 <b style="color: white; font-size: 18px;">{weather['temp']}°C</b><br>
-                <span style="color: white; font-size: 15px; opacity: 0.8;">Ressenti : {ressenti}°C</span></br>
+                <span style="color: white; font-size: 15px; opacity: 0.8;">Ressenti : {ressenti}°C</span><br>
                 <span style="color: white; font-size: 12px; opacity: 0.8;">Vent : {weather['wind']}</span>
             </div>
         </div>{alert_html}{msc_html}
