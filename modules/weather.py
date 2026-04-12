c3e8bae0070f70041bc6171e1081d3c7f3a71b35
momoF07 <little.skylander@gmail.com>
2026-04-12 20:00:38 +0200

    ?

--- modules/weather.py
+++ modules/weather.py
@@ -1,7 +1,6 @@
 import requests
 import streamlit as st
 
-@st.cache_data(ttl=150)
 def get_disney_weather():
     lat, lon = 48.8675, 2.7841
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m,wind_gusts_10m&timezone=Europe%2FParis"
