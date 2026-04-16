5beddf05071ccb81249de134b11e8399ca7e8d62
momoF07 <little.skylander@gmail.com>
2026-04-17 00:54:58 +0200

    .

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if not df_live.empty and not df_pannes_brutes.empty:
         })
 
 # --- BLOC METEO ---
-render_weather_card(get_disney_weather())
+#render_weather_card(get_disney_weather())
 render_weather_card(get_maintenance_weather())
 
 # --- HEADER INFO ---
