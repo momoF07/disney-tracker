93037d563276b44a30432c64c4576ba8e8df8678
momoF07 <little.skylander@gmail.com>
2026-05-05 17:44:07 +0200

    fix

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ if not df_live.empty:
 
 # --- SECTION FLUX & STATISTIQUES ---
 st.write("---")
-col_flux, col_stats = st.columns([1.5, 1], gap="large")
+col_flux, col_stats = st.columns([1, 1.5], gap="large")
 
 with col_flux:
     st.subheader("🚨 Flux du jour")
