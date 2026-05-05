853312c3356f14bb46131b234e68e84b324878c0
momoF07 <little.skylander@gmail.com>
2026-05-05 16:50:11 +0200

    v3

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from ui.filters import render_quick_filters
 from maintenance import show_maintenance
 
 # --- VERSION ---
-webversion = "v2.5"
+webversion = "v3"
 
 # --- RÉFÉRENTIEL DES ATTRACTIONS ---
 ALL_RIDES_LIST = sorted(list(set(RIDES_DLP + RIDES_DAW)))
