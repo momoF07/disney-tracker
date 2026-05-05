9666e57a5fe0121446ece0902859728c7c297384
momoF07 <little.skylander@gmail.com>
2026-05-05 15:09:16 +0200

    fix show

--- ui/comp.py
+++ ui/comp.py
@@ -135,7 +135,7 @@ def render_upcoming_shows(schedules):
 
     if not schedules: return
 
-    HIDDEN_SHOWS = ["philharmagic", "reserved viewing", "animation academy"]
+    HIDDEN_SHOWS = ["mickey's philharmagic", "reserved viewing area", "animation academy"]
 
     PARKS = [
         {"label": "Disneyland Park", "prefix": "Disneyland Park", "color": "#ffb3d1"},
